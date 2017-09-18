e3f3cdcade9495c849ec6b418e4315940a3a7b03
Brandon Mathis <mathisbb@gmail.com>
2017-09-18 16:59:13 -0400

    Implemented first iteration of ServerProgram. We still need to make sure that the server and client agree on port number.

--- src/edu/unf/cnt4504/server/ServerProgram.java
+++ src/edu/unf/cnt4504/server/ServerProgram.java
@@ -1,5 +1,25 @@
 package edu.unf.cnt4504.server;
 
-public class ServerProgram {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+/**
+ * Contains the main method that our server will run.
+ * It iteratively instantiates new servers to serve clients in sequence.
+ * @author Brandon
+ *
+ */
+public class ServerProgram {
+	public static void main(String[] args) {
+		//The port that the program will listen on
+		final int port = 8080;
+		
+		try (ServerSocket socket = new ServerSocket(port)) {
+			while (true) {
+				new ChildServer(socket).run();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
