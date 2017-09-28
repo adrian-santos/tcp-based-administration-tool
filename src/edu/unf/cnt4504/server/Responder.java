cd913ed22855caef635ba7207295eaefce5727ec
Sotir Kolvani <n00845431@ospreys.unf.edu>
2017-09-28 17:52:21 -0400

    responder

--- src/edu/unf/cnt4504/server/Responder.java
+++ src/edu/unf/cnt4504/server/Responder.java
@@ -1,92 +1,34 @@
 package edu.unf.cnt4504.server;
 
 public class Responder {
-    public getResponse (String Message) {
-        return getResponse;
+    public getResponse (String message) {
 
-        switch (caseNum) {
+
+        switch (message) {
+
             case 1:
-                someRuntime = Runtime.getRuntime();
                 cmd = "date";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
 
             case 2:
-
-                // Creates a process for running processes
-                someRuntime = Runtime.getRuntime();
                 cmd = "uptime";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
             case 3:
-
-                // Creates a process for running processes
-                someRuntime = Runtime.getRuntime();
                 cmd = "free";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
             case 4:
-
-                // Creates a process for running processes
-                someRuntime = Runtime.getRuntime();
                 cmd = "netstat";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
             case 5:
-
-                // Creates a process for running processes
-                someRuntime = Runtime.getRuntime();
                 cmd = "who";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
             case 6:
-
-                // Creates a process for running processes
-                someRuntime = Runtime.getRuntime();
                 cmd = "ps -e";
-
-                // Runs the process, gets the ouptut, and prints it on the client side
-                sProcess = sRuntime.exec(cmd);
-                stdInp = (new InputStreamReader(someProcess.getInputStream()));
-
-                // Sets the output to blank so that all previous outputs will not be concatenated
-                output = "";
                 break;
 
             case 7:
@@ -96,5 +38,13 @@ public class Responder {
 
 
         }
+
+        someRuntime = Runtime.getRuntime();
+        // Runs the process, gets the ouptut, and prints it on the client side
+        sProcess = sRuntime.exec(cmd);
+        stdInp = (new InputStreamReader(someProcess.getInputStream()));
+
+        // Sets the output to blank so that all previous outputs will not be concatenated
+        output = "";
     }
 }
