28ff7b519907dc037d64336aee6e1206144fcf8f
Adrian Santos <iancris31@yahoo.com>
2017-09-28 18:14:21 -0400

    Update Menu.java
    Edited with Kevan

--- src/edu/unf/cnt4504/client/Menu.java
+++ src/edu/unf/cnt4504/client/Menu.java
@@ -13,8 +13,8 @@ public class Menu {
 	public static void main(String[] args) {
 
 		int command = 0; // user option
-		int loop; // Continue?
-		float time = 0; // mean server response time
+		int loop = 1; // Continue?
+		long time = 0; // mean server response time
 
 		Scanner input = new Scanner(System.in);
 
@@ -25,18 +25,12 @@ public class Menu {
 
 		String server = args[0];
 
-		// //
-		System.out.println("Server: " + server); // TEST
-		// //
+		ClientParallelizer cp = new ClientParallelizer(server, 8080, 2);
+		Message msg = new Message();
 
 		do {
-
-			loop = 1; // Continue.
-
-			System.out
-					.println("This is a client program that communicates with a server.");
-			System.out
-					.println("Please enter a number to request information from the server.");
+			System.out.println("This is a client program that communicates with a server.");
+			System.out.println("Please enter a number to request information from the server.");
 			System.out.println("Options:");
 			System.out.println("1. Host current Date and Time");
 			System.out.println("2. Host uptime");
@@ -64,57 +58,48 @@ public class Menu {
 				continue;
 			}// Check user input for invalid digit.
 
-			else if (command == 7) {
-				loop = 0;
-				continue;
-			}// Check for quit command.
-
 			// Process information request.
-			// //
-			else if (command == 1) {
-				// METHOD CALL TO GET DATE AND TIME
-				System.out.println("Date and Time command accepted.");// TEST
+			switch(command) {
+				case 7: // Check for quit command.
+					loop = 0;
+					continue;
+				case 1: // GET DATE AND TIME
+					cp.setRequest(msg.CURRENT_DATE_AND_TIME);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println(response);
+					break;
+				case 2: // GET UPTIME
+					cp.setRequest(msg.UPTIME);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println("Uptime: " + response);
+					break;
+				case 3: // GET MEMORY USE
+					cp.setRequest(msg.MEMORY_USE);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println("Memory Usage: " + response);
+					break;
+				case 4: // GET NETSTAT
+					cp.setRequest(msg.NETSTAT);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println("NETSTAT: " + response);
+					break;
+				case 5: // GET CURRENT USERS
+					cp.setRequest(msg.CURRENT_USERS);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println("Current Users: " + response);
+					break;
+				case 6: // GET RUNNING PROCESSES
+					cp.setRequest(msg.RUNNING_PROCESSES);
+					cp.runClients();
+					String response = cp.getResponse();
+					System.out.println("Running Processes: " + response);
+					break;
 			}
-			// //
-
-			// //
-			else if (command == 2) {
-				// METHOD CALL TO GET UPTIME
-				System.out.println("Uptime command accepted.");// TEST
-			}
-			// //
-
-			// //
-			else if (command == 3) {
-				// METHOD CALL TO GET MEMORY USE
-				System.out.println("Memory use command accepted.");// TEST
-			}
-			// //
-
-			// //
-			else if (command == 4) {
-				// METHOD CALL TO GET NETSTAT
-				System.out.println("Netstat command accepted.");// TEST
-			}
-			// //
-
-			// //
-			else if (command == 5) {
-				// METHOD CALL TO GET CURRENT USERS
-				System.out.println("Current users command accepted.");// TEST
-			}
-			// //
-
-			// //
-			else if (command == 6) {
-				// METHOD CALL TO GET RUNNING PROCESSES
-				System.out.println("Running processes command accepted.");// TEST
-			}
-			// //
-
-			// GET RESPONSE.
-
-			// DISPLAY RESPONSE.
 
 			// GET MEAN SERVER RESPONSE TIME.
 			System.out.println("Mean server response time: " + time);
