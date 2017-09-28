3028eddd33b553da2a82b58f0b3104cc4a81914e
Adrian Santos <iancris31@yahoo.com>
2017-09-28 18:17:50 -0400

    Update ClientParallelizer.java
    Updated file

--- src/edu/unf/cnt4504/client/ClientParallelizer.java
+++ src/edu/unf/cnt4504/client/ClientParallelizer.java
@@ -17,90 +17,48 @@ import java.util.*;
 import java.net.*;
 
 public class ClientParallelizer {
-	
-    public ClientParallelizer(int hostAddress, int port, int client){
+
+    List<Client> clients = new ArrayList<>();
+
+    public ClientParallelizer(InetAddress hostName, int port, int client){
         //Check Host name and ports they connect to
-        this.hostAddress = hostAddress;
-        this.port = port;
-        this.client = client;
-    }
-    
-    public countClients(){
-      //Count clients
-        int clients = 0; //Change variable count
-      
-        for(int i = 0; i > clients.count; i++){
-            clients += 1;
+        //Count clients
+        for(int i = 0; i < client;i++ ){
+            Client clientNumber = new Client(hostName, port);
+            clients.add();
         }
-        
-        System.out.println("The total number of clients is" + clients);
-    
     }
-    
-    //Host name checker
-	public hostName(){
-	
-	
-	}
 
-    
-   
     //Calculates the mean server response time and response for a given request
-    
-     /* * * * * * * * Test Code 1 * * * * * * * */
-    long[] arrayTimes = new long[50];
-    int numTimes;
-
-
-    public new Client() {
-        numTimes = 0;
-    }
-
-    public synchronized void push (long time) {
-        arrayTimes[numTimes] = time;
-        numTimes++;
-    }
-
-
-
-
-    public double getAverage () {
-        double averageTime = 0;
-
-        for (int count = 0; count < numTimes; count++)
-            averageTime += (double)arrayTimes[count];
-
-        averageTime = averageTime / (double)arrayTimes.length;
-
-        return averageTime;
-
-    }
-    
-    
-    
-    /* * * * * * * * Test Code 2 * * * * * * * */
-   long[] times;
-   int counter;
-
-	public threadTime(int n) {
-      times = new long[n];
-	  counter = 0;
+    //Host name checker
+	public void setRequest(String request){
+        // for each loop
+        for (Client client: clients) {
+            client.setRequest(request)
+        }
 	}
 
-	public synchronized void add (long t) {
-		times[counter++] = t;
-	}
-	
-	
-	public double getAverage (int num) {
-		double avg = 0;
-		
-		for (int i = 0; i < num; i++)
-			avg += (double)times[i];
-	
-		   avg = avg / (double)times.length;
-		
-		return avg;
-	}	
+	public void runClients(){
+        for(Client client: clients){
+            client.run();
+        }
+    }
+
+    public long getMeanResponseTime(){
+	    //Response Time
+	    long responseTime = 0;
+
+        for(Client client: clients){
+            responseTime += client.getResponseTime();
+        }
+
+        responseTime /= client;
+
+        return responseTime;
+    }
+
+    public String getResponse(){
+
+    }
 
 }
